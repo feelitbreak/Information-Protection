770f266827a5e4f51e47e53ebf78031991f113c0
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-02-26 17:57:58 +0300

    lab1 substitution cipher

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,11 @@
-# Лабораторная работа №1. Вариант 8. Кендысь Алексей
+# Лабораторная работа №1. Вариант 8. Кендысь Алексей, 3 курс, 7 группа.
 
 alphabet = [
     'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п',
     'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
 ]
+
+default_key2 = 'чтаюшднысхфьуёэцвеклиъямзойгжщрбп'
 
 
 def get_num(letter):
@@ -14,21 +16,52 @@ def get_letter(num):
     return alphabet[num]
 
 
-def input_text_key():
-    print("Input text to be encrypted.")
-    in1 = input()
-    print("Input key.")
-    in2 = input()
+def input_text_key1():
+    in1 = input('Input text to be encrypted: ')
+    in2 = input('Input key: ')
+    return in1, in2
+
+
+def input_text_key2():
+    in1 = input('Input ciphertext to be decrypted: ')
+    in2 = input('Input key (if invalid, default is used): ')
+
+    if len(in2) != len(alphabet):
+        print('Invalid key. Using default key: ' + default_key2)
+        in2 = default_key2
+
     return in1, in2
 
 
 def vigenere():
     ciphertext_list = []
-    for i in range(len(plaintext)):
-        num1 = get_num(plaintext[i])
-        num2 = get_num(key[i % len(key)])
+    for i in range(len(plaintext1)):
+        num1 = get_num(plaintext1[i])
+        num2 = get_num(key1[i % len(key1)])
         num = (num1 + num2) % len(alphabet)
-        ciphertext_list += get_letter(num)
+        ciphertext_list.append(get_letter(num))
+
+    return ''.join(ciphertext_list)
+
+
+def get_sigma():
+    sigma = [[], []]
+    for i in range(len(alphabet)):
+        sigma[0].append(i)
+        key_num = get_num(key2[i])
+        sigma[1].append(key_num)
+
+    return sigma
+
+
+def substitution():
+    sigma = get_sigma()
+    ciphertext_list = []
+    for letter in plaintext2:
+        num = get_num(letter)
+        ind = sigma[1].index(num)
+        cipher_num = sigma[0][ind]
+        ciphertext_list += get_letter(cipher_num)
 
     return ''.join(ciphertext_list)
 
@@ -36,5 +69,8 @@ def vigenere():
 # main
 if __name__ == "__main__":
     print("Task 1. Encryption, Vigenere cipher.")
-    plaintext, key = input_text_key()
+    plaintext1, key1 = input_text_key1()
     print("Ciphertext: " + vigenere())
+    print("Task 2. Decryption, simple substitution cipher.")
+    plaintext2, key2 = input_text_key2()
+    print("Decrypted text: " + substitution())

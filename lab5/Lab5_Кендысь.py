087651f592f263d3b47b79cfb4611d98521bc4d4
feelitbreak <88198704+feelitbreak@users.noreply.github.com>
2023-04-13 14:03:05 +0300

    lab5 sign function

--- lab5/Lab5_Кендысь.py
+++ lab5/Lab5_Кендысь.py
@@ -1,8 +1,10 @@
 # Лабораторная работа №5. Вариант 8. Кендысь Алексей, 3 курс, 7 группа.
 
 import random
+from hashlib import sha256
 
 q = 204549505434169694705359613953840507945808782458714026455216935639206129681467
+m = "I, Alexey Kendys, love MiKOZI"
 
 
 def extended_euclidian(a, b):
@@ -23,6 +25,15 @@ def extended_euclidian(a, b):
     return prev_x, prev_y, a
 
 
+def get_reverse(num, mod):
+    res, unused, gcd = extended_euclidian(num, mod)
+
+    if res < 0:
+        res += mod
+
+    return res
+
+
 def fast_pow(num, power, mod):
     num %= mod
     res = 1
@@ -41,23 +52,35 @@ def fast_pow(num, power, mod):
 
 def gen(in_q):
     while True:
-        r = random.randrange(2, 4 * (in_q + 1), 2)
-        in_p = in_q * r + 1
-        if fast_pow(2, in_q * r, in_p) == 1 and fast_pow(2, r, in_p) != 1:
+        in_r = random.randrange(2, 4 * (in_q + 1), 2)
+        out_p = in_q * in_r + 1
+        if fast_pow(2, in_q * in_r, out_p) == 1 and fast_pow(2, in_r, out_p) != 1:
             break
 
     while True:
-        x = random.randrange(in_p)
-        in_g = fast_pow(x, r, in_p)
-        if in_g != 1:
+        x = random.randrange(out_p)
+        out_g = fast_pow(x, in_r, out_p)
+        if out_g != 1:
             break
 
-    in_d = random.randrange(in_q)
-    in_e = fast_pow(in_g, in_d, in_p)
+    out_d = random.randrange(in_q)
+    out_e = fast_pow(out_g, out_d, out_p)
 
-    return (in_p, in_q, in_g), in_e, in_d
+    return (out_p, in_q, out_g), out_e, out_d
+
+
+def sign(in_p, in_q, in_g, in_d, in_m):
+    h = sha256(in_m.encode("utf8"))
+    m1 = int(h.hexdigest(), base=16)
+
+    k = random.randrange(1, in_q)
+    out_r = fast_pow(in_g, k, in_p)
+    out_s = (get_reverse(k, in_q) * ((m1 - in_d * out_r) % in_q)) % in_q
+
+    return out_r, out_s
 
 
 # main
 if __name__ == "__main__":
     (p, q, g), e, d = gen(q)
+    r, s = sign(p, q, g, d, m)
